6d3881d3020de7c504b6ae57f4c923eaf6cf9cbe
Funson Lee <funson86@gmail.com>
2014-09-11 17:23:00 +0800

    modify css file

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -17,7 +17,7 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = ['css/site.css'];
+    public $css = ['css/admin.css'];
     public $js = [];
     public $depends = [
         'yii\web\YiiAsset',
