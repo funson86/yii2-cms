8f65f48a20bd659ec043981d90507bf8599b6e91
花生 <funson86@gmail.com>
2015-03-13 11:03:21 +0800

    Update CmsCatalogSearch.php

--- models/CmsCatalogSearch.php
+++ models/CmsCatalogSearch.php
@@ -5,7 +5,6 @@ namespace funson86\cms\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\CmsCatalog;
 
 /**
  * CmsCatalogSearch represents the model behind the search form about `app\models\CmsCatalog`.
